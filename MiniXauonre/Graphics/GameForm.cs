84f5c4f0fd22149aca24e16ce1b16681c9cfd4a9
tupotrof <supertrof101@gmail.com>
2018-05-11 17:18:55 +0500

    Player names in forms

---
+++ MiniXauonre/Graphics/GameForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MiniXauonre.Controller;
+using MiniXauonre.Core;
+
+namespace MiniXauonre.Graphics
+{
+    public class GameForm : Form
+    {
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            DoubleBuffered = true;
+            //WindowState = FormWindowState.Maximized;
+        }
+
+        public GameForm()
+        {
+            var w = 5;
+            var l = 5;
+            var heroesNumber = 1;
+            var pl1Spawn = new Xauonre.Core.Point(0, 0);
+            var pl2Spawn = new Xauonre.Core.Point(l - 1, w - 1);
+
+            var nameForm = new PlayerNameForm();
+            Application.Run(nameForm);
+            var p1 = nameForm.Player1Name;
+            var p2 = nameForm.Player2Name;
+
+            var game = new Game(new List<Tuple<string, Xauonre.Core.Point>>{ Tuple.Create(p1, pl1Spawn),
+                Tuple.Create(p2, pl2Spawn) },
+                heroesNumber,
+                l,
+                w);
+            //foreach (var player in game.Players)player.AllDamage = 1000000000;
+            game.StartGame();
+        }
+    }
+}
--- MiniXauonre/Program.cs
+++ MiniXauonre/Program.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xauonre.Core;
+using MiniXauonre.Graphics;
+using System.Windows.Forms;
 
 namespace MiniXauonre
 {
@@ -14,24 +16,8 @@ namespace MiniXauonre
     {
         static void Main(string[] args)
         {
-
-            var w = 5;
-            var l = 5;
-            var heroesNumber = 1;
-            var pl1Name = Console.ReadLine();
-            var pl1Spawn = new Point(0, 0);
-            var pl2Name = Console.ReadLine();
-            var pl2Spawn = new Point(l - 1, w - 1);
-
-            var game = new Game(new List<Tuple<string, Point>>{ Tuple.Create(pl1Name, pl1Spawn),
-                Tuple.Create(pl2Name, pl2Spawn) },
-                heroesNumber,
-                l,
-                w);
-            //foreach (var player in game.Players)player.AllDamage = 1000000000;
-            game.StartGame();
-
-            while (true) { }
+            var gameForm = new GameForm();
+            Application.Run(gameForm);
         }
     }
 }
